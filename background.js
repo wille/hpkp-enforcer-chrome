c3aed3ffc44fabb71d2259cd64f6a446487e5bc9
redpois0n <red@cmail.nu>
2015-12-12 14:16:51 +0100

    post to report-uri

--- background.js
+++ background.js
@@ -2,6 +2,30 @@ var ignore = [
 
 ];
 
+function post() {
+	post("https://report-uri.io/#", { "url": url });
+}
+
+function post(path, params) {
+	var form = document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", path);
+
+    for (var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+
 // Default max-age 6 months in seconds
 var max_age = "15570000";
 
