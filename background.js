0c94a08c90b00a1baa43b8bfe281ba82ee62d924
redpois0n <red@cmail.nu>
2015-12-20 18:08:03 +0100

    Get pins from local storage

--- background.js
+++ background.js
@@ -5,13 +5,23 @@ chrome.webRequest.onHeadersReceived.addListener(
 	function(details) {
 		var url = new URL(details.url);
 		
-		if (url.protocol === "https:") {
+		var pending = localStorage["pending"];
+		
+		if (pending.indexOf(url.hostname) != -1 && url.protocol === "https:") {
+			var pins = pending[url.hostname];
+			
 			for (var i = 0; i < details.responseHeaders.length; i++) {
 				if (details.responseHeaders[i].name.toLowerCase() === "public-key-pins")
 				return { };
 			}
 			
-			var header = "max-age=" + max_age;
+			var header = "";
+
+			for (var pin in pending) {
+				header += "pin-sha256=\"" + pin "\";";
+			}
+			
+			header += "max-age=" + max_age;
 
 			details.responseHeaders.push({
 				"name": "Public-Key-Pins",
