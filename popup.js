4ad28b9d4d0d6c43c9ccff9312a67e4ca93b7843
redpois0n <red@cmail.nu>
2015-12-20 18:24:31 +0100

    popup_pins iframe

--- popup.js
+++ popup.js
@@ -1,22 +1,9 @@
-var grabber_url = "https://redpois0n.com/pin.php";
 
-document.addEventListener("DOMContentLoaded", function() {
-	var checkPageButton = document.getElementById("addpage");
-	checkPageButton.addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function() {	
+	var addPageButton = document.getElementById("addpin");
+	addPageButton.addEventListener("click", function() {
 		chrome.tabs.getSelected(null, function(tab) {
-			var x = document.createElement("a");
-			x.href = tab.url;
 			
-			var form = document.createElement("form");
-			form.action = grabber_url;
-			form.method = "post";
-			var i = document.createElement("input");
-			i.type = "hidden";
-			i.name = "url";
-			i.value = x.hostname;
-			form.appendChild(i);
-			document.body.appendChild(form);
-			form.submit();
 		});
 	}, false);
 }, false);
