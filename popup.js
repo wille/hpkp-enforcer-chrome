98505054cbab468ec746655c2bd3e46b63e840e4
redpois0n <red@cmail.nu>
2015-12-20 18:36:25 +0100

    Remove *_pins*, send data using GET and put response text (the pin) in the textbox

--- popup.js
+++ popup.js
@@ -1,9 +1,22 @@
+function geturl(host) {
+	return "https://redpois0n.com/pin.php?url=" + host;
+}
 
 document.addEventListener("DOMContentLoaded", function() {	
-	var addPageButton = document.getElementById("addpin");
+	
+	var addPageButton = document.getElementById("load");
 	addPageButton.addEventListener("click", function() {
 		chrome.tabs.getSelected(null, function(tab) {
-			
+			chrome.tabs.getSelected(null, function(tab) {
+				var x = document.createElement("a");
+				x.href = tab.url;
+				
+				var req = new XMLHttpRequest();
+				req.open("GET", geturl(x.hostname), false);
+				req.send(null);
+				document.getElementById("pinbox").value = req.responseText;
+			});
 		});
 	}, false);
+	
 }, false);
