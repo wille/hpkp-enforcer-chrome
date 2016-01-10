1ebde18a6762840e67d4b6c6daa627aaff14c613
redpois0n <red@cmail.nu>
2016-01-10 20:51:40 +0100

    Fake pin added, works

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # HTTP Public Key Pinning
 
-Plugin made for Google Chrome where the user can manually associate public keys with a certain web server 
+Plugin made for Google Chrome where the user can manually associate public keys with a certain web server
 without the web server sending out the HPKP header
 
-Web service to get pins located [here](https://github.com/redpois0n/hpkp-enforcer-web), you might also want to 
+Web service to get the pin is located [here](https://github.com/redpois0n/hpkp-enforcer-web), you might also want to
 visit [report-uri.io](https://report-uri.io)
+
+Since Chrome requires atleast 2 pins, and the second one is required to be outside the first certificate chain, we cannot get retrieve any valid pin for this, so we use a fake one. See below.
+
+# Generating the fake backup pin
+
+```
+$ echo "" | openssl dgst -sha256 -binary | base64 > fake.pin
+```
--- background.js
+++ background.js
@@ -4,26 +4,27 @@ var max_age = "15570000";
 chrome.webRequest.onHeadersReceived.addListener(
 	function(details) {
 		var url = new URL(details.url);
-		
+
 		var json = localStorage[url.hostname];
-		
+
 		if (json != undefined && url.protocol === "https:") {
-			var pins = JSON.parse(json);
-			
+			var pins = JSON.parse(json); // The web server should return 1 pin. We need 2, the second is a backup key which needs to be outside the main keys certificate chain, which we don't have so we'll just add a fake one
+			pins.push("AbpHGcgLb+kRsJGnwFEktk7uzpZOCcBY74+YBdrKVGs=");
+
 			var header = "";
-			
+
 			for (var i = 0; i < pins.length; i++) {
 				header += "pin-sha256=\"" + pins[i] + "\"; ";
 			}
-			
+
 			header += "max-age=" + max_age;
 			console.log(header);
-			
+
 			details.responseHeaders.push({
 				"name": "Public-Key-Pins",
 				"value": header
 			});
-			
+
 			delete localStorage[url.hostname];
 		}
 
